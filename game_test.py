1ccea15d5b7f776d6a723a5fb94c6d89c672ae8e
geekofalltrades <thegeekofalltrades@gmail.com>
2014-03-04 20:43:09 -0800

    Began implementing tests for overhauled game class. Tests for _get_next_player and _get_previous_player are written and are passing.

--- game_test.py
+++ game_test.py
@@ -1,16 +1,15 @@
 import unittest
 from game import Game
 
-NAMES = ['Jordan', 'Matt', 'Justin', 'Cris']
-
 
 class GameTest(unittest.TestCase):
 
     def setUp(self):
         self.game = Game()
+        self.names = ['Jordan', 'Matt', 'Justin', 'Cris']
 
     def setUp_players(self):
-        for name in NAMES:
+        for name in self.names:
             self.game.add_player(name)
 
     def test_player_size(self):
@@ -19,27 +18,74 @@ class GameTest(unittest.TestCase):
     def test_add_player(self):
         self.test_player_size()
         #Checks for size
-        for name in NAMES:
+        for name in self.names:
             self.game.add_player(name)
             self.test_player_size()
-        for index in range(len(NAMES)):
-            self.assertEquals(NAMES[index],
+        for index in range(len(self.names)):
+            self.assertEquals(self.names[index],
                               self.game.players_list[index].name)
 
-    def test_mod(self):
-        self.setUp_players()
-        for i in range(len(NAMES)):
-            for j in range(1, 4):
-                players = i
-                self.assertEquals(
-                    self.game.mod(players, j),
-                    (i + j) % self.game.players_size)
 
-    def test_run_round(self):
-        self.setUp_players()
-        self.game.last_raise = 1
-        self.game.players_list[3].active = False
-        self.game.run_round()
+class TestPlayerGetFunctions(unittest.TestCase):
+    """Test the _get_next_player and _get_previous_player funcions."""
+    def setUp(self):
+        self.game = Game()
+        for name in ['Jordan', 'Matt', 'Justin', 'Cris']:
+            self.game.add_player(name)
+
+        self.middle_next = 1
+        self.middle_prev = 2
+        self.beginning = 0
+        self.end = 3
+
+    def test_get_next_from_center(self):
+        """Get the next player from the middle of the list of players."""
+        self.assertEqual(self.game._get_next_player(self.middle_next),
+            self.middle_next + 1)
+
+    def test_get_next_from_center_with_step(self):
+        """Get the next player from the middle of the list of players
+        using a step size greater than one.
+        """
+        self.assertEqual(self.game._get_next_player(self.middle_next, 2),
+            self.middle_next + 2)
+
+    def test_get_next_from_end(self):
+        """Get the next player from the end of the list of players."""
+        self.assertEqual(self.game._get_next_player(self.end),
+            self.beginning)
+
+    def test_get_next_from_end_with_step(self):
+        """Get the next player from the end of the list of players using
+        a step size greater than one.
+        """
+        self.assertEqual(self.game._get_next_player(self.end, 2),
+            self.beginning + 1)
+
+    def test_get_previous_from_center(self):
+        """Get the previous player from the middle of the list of players."""
+        self.assertEqual(self.game._get_previous_player(self.middle_prev),
+            self.middle_prev - 1)
+
+    def test_get_previous_from_center_with_step(self):
+        """Get the previous player from the middle of the list of players
+        using a step size greater than one.
+        """
+        self.assertEqual(self.game._get_previous_player(self.middle_prev, 2),
+            self.middle_prev - 2)
+
+    def test_get_previous_from_beginning(self):
+        """Get the previous player from the beginning of the list of players.
+        """
+        self.assertEqual(self.game._get_previous_player(self.beginning),
+            self.end)
+
+    def test_get_previous_from_beginning_with_step(self):
+        """Get the previous player from the beginning of the list of players
+        using a step size greater than one.
+        """
+        self.assertEqual(self.game._get_previous_player(self.beginning, 2),
+            self.end - 1)
 
 
 if __name__ == '__main__':
