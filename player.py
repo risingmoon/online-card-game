b6950ec0becc683e01014e5770498b2bff0f518f
geekofalltrades <thegeekofalltrades@gmail.com>
2014-03-04 16:58:03 -0800

    Changing the Player class to not know stuff about itself that the game assigns to it. Instead, the game will keep track of roles it has assigned to each player that affect relations between players, and the Player class will only track things about itself that don't directly affect interactions between other players.

--- player.py
+++ player.py
@@ -9,26 +9,7 @@ class Player:
         self.points = points
         self.bet = 0
         self.active = True
-
-        #Features that the game assigns to the player. These (along with
-        #self.bet, above) are set at the beginning of each round.
         self.hand = None
-        self.turn = False
-        self.big_blind = False
-        self.small_blind = False
-        self.dealer = False
-
-    def clear_round_attributes(self):
-        """This function allows the game to easily clear attributes on
-        the player that are specific to a certain round, so that the
-        player can be made ready for the next round.
-        """
-        self.bet = 0
-        self.hand = None
-        self.turn = False
-        self.big_blind = False
-        self.small_blind = False
-        self.dealer = False
 
     def call(self, points):
         self.points -= points
