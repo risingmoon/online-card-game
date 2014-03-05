cbe58327446c6f42a5e233d7bab35f7435f14bf5
geekofalltrades <thegeekofalltrades@gmail.com>
2014-03-04 20:16:58 -0800

    Fixed a couple typos in the player_test file. These tests are still sufficient; it's the game that's going to be a real nightmare to test.

--- player_test.py
+++ player_test.py
@@ -14,13 +14,13 @@ class PlayerTest(unittest.TestCase):
     def test_call(self):
         self.player.call(25)
         self.assertEqual(self.player.bet, 25)
-        self.assertEquals(self.player.points, 75)
+        self.assertEqual(self.player.points, 75)
 
     def test_raise_bet(self):
         self.test_call()
         self.player.raise_bet(50)
         self.assertEqual(self.player.bet, 75)
-        self.assertEquals(self.player.points, 25)
+        self.assertEqual(self.player.points, 25)
 
     def test_fold(self):
         self.test_raise_bet()
