3891a63762d274eb1e7bee3be58b09870ea41dcf
Jordan Brengman <jordanbrengman@gmail.com>
2014-03-05 10:03:35 -0800

    Add prime number values for card

--- card.py
+++ card.py
@@ -1,8 +1,9 @@
 class Card(object):
 
-    def __init__(self, value, suit, string):
+    def __init__(self, value, suit, prime, string):
         self.value = value
         self.suit = suit  # 1,2,3,4 -> Clubs, Diamonds, Hearts, Spades
+        self.prime = prime
         self.string = string
 
     def __str__(self):
