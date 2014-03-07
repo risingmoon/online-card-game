ca753f75d97a3b7d8711ff79bae5ee9780a91491
risingmoon <justindavidlee88@gmail.com>
2014-03-07 00:25:37 -0800

    Reorganized files.

--- install.sh
+++
@@ -1,9 +0,0 @@
-
-filename="packages.txt"
-packages=""
-while read -r package;do
-    echo "here $package";
-    packages="$packages $package";
-done < $filename
-sudo apt-get update
-sudo apt-get install$packages
